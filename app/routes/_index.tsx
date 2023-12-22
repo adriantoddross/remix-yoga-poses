911653c699b555930bc1a9b40dbf5f5ca4629365
Adrian Ross <adriantoddross@gmail.com>
2023-12-22 13:34:13 -0800

    Stub homepage

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,40 +2,67 @@ import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Yoga Poses" },
+    { name: "description", content: "Learn new yoga poses!" },
   ];
 };
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div>
+      <header>
+        <h1>Yoga Poses</h1>
+        <nav>
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Login</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+      <div>
+        <div>
+          <fieldset>
+            <legend>
+              <h2>Filters</h2>
+            </legend>
+
+            <label>
+              Pose category 1
+              <input type="checkbox" name="" id="" />
+            </label>
+            <label>
+              Pose category 2
+              <input type="checkbox" name="" id="" />
+            </label>
+            <label>
+              Pose category 3
+              <input type="checkbox" name="" id="" />
+            </label>
+          </fieldset>
+        </div>
+        <div>
+          <h2>Poses</h2>
+          <ul>
+            <li>
+              <h3>Pose title</h3>
+              <p>Pose description/instruction</p>
+              <div>Image</div>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <footer>
+        <p>Created by Adrian Ross</p>
+      </footer>
     </div>
   );
 }
