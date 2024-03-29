a1e06e6be2c478ffd3bc7366da06f7c4ebdbc69a
Adrian Ross <adriantoddross@gmail.com>
2024-03-28 19:26:48 -0700

    Add new type for poses and categories

---
+++ app/types/index.ts
@@ -0,0 +1,21 @@
+export type PoseRecord = {
+  createdAt: string;
+  id: number;
+  english_name: string;
+  sanskrit: string;
+  sanskrit_name_adapted: string;
+  sanskrit_name: string;
+  translation_name: string;
+  pose_description: string;
+  pose_benefits: string;
+  url_svg: string;
+  url_png: string;
+  url_svg_alt: string;
+};
+
+export type PoseCategory = {
+  id: number;
+  category_name: string;
+  category_description: string;
+  poses: PoseRecord[];
+};
