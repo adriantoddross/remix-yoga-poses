6c859a44f92ea477bbd1411aff138c8fcdc3f586
Adrian Ross <adriantoddross@gmail.com>
2024-03-28 19:53:02 -0700

    Display pose name and picture

--- app/components/Pose.tsx
+++ app/components/Pose.tsx
@@ -1,10 +1,14 @@
+import { Link } from "@remix-run/react";
 import { PoseRecord } from "~/types";
 
-export default function Pose({ english_name, pose_description }: PoseRecord) {
+export default function Pose({ english_name, url_svg }: PoseRecord) {
   return (
     <li>
       <h3>{english_name}</h3>
-      <p>{pose_description}</p>
+      <img src={url_svg} alt={`Graphic of person doing ${english_name} pose`} />
+      <div>
+        <Link to={`/poses/${english_name}`}>Learn more</Link>
+      </div>
     </li>
   );
 }
