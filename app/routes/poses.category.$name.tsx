3c94721d1bbabf00517c65af1d3754fbc61e924b
Adrian Ross <adriantoddross@gmail.com>
2024-03-28 19:30:09 -0700

    Show category and pose

---
+++ app/routes/poses.category.$name.tsx
@@ -0,0 +1,39 @@
+import { LoaderFunctionArgs, json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { PoseCategory } from "~/types";
+
+export async function loader({ params }: LoaderFunctionArgs) {
+  console.log(
+    `${process.env.YOGA_API_BASE_URL}${process.env.YOGA_API_CATEGORIES}?name=${params.name}`
+  );
+
+  const getCategory = await fetch(
+    `${process.env.YOGA_API_BASE_URL}${process.env.YOGA_API_CATEGORIES}?name=${params.name}`
+  ).then((data) => data);
+
+  const category: PoseCategory = await getCategory.json();
+
+  return json({ category });
+}
+
+export default function Category() {
+  const { category } = useLoaderData<typeof loader>();
+  const { category_name, category_description, poses } = category;
+
+  return (
+    <section>
+      <h2>{category_name}</h2>
+      <p>{category_description}</p>
+      <ul>
+        {poses.map(({ id, english_name, pose_description }) => {
+          return (
+            <li key={id}>
+              <h3>{english_name}</h3>
+              <p>{pose_description}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
+}
