a8948d1842d3af804763985fa78d88347988450a
Adrian Ross <adriantoddross@gmail.com>
2024-04-10 17:01:07 -0700

    Remove unused type

--- app/components/Pose.tsx
+++ app/components/Pose.tsx
@@ -1,25 +1,19 @@
 import { Link } from "@remix-run/react";
 import { PoseRecord } from "~/types";
 
-type PoseProps = PoseRecord & {
-  handleFavoriteClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
-};
-
-export default function Pose({
-  english_name,
-  id,
-  url_svg,
-  handleFavoriteClick,
-}: PoseProps) {
+export default function Pose({ english_name, id, url_svg }: PoseRecord) {
   return (
     <li>
       <h3>{english_name}</h3>
       <img src={url_svg} alt={`Graphic of person doing ${english_name} pose`} />
       <div>
         <Link to={`/poses/${english_name}`}>Learn more</Link>
-        <button type="button" onClick={handleFavoriteClick} value={id}>
-          Favorite
-        </button>
+        <label>
+          <button type="submit" name="pose_id" value={id}>
+            {" "}
+            Favorite pose
+          </button>
+        </label>
       </div>
     </li>
   );
