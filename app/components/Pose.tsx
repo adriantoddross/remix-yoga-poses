24d4be79a102bba9b277cbc5f032969760ea68a5
Adrian Ross <adriantoddross@gmail.com>
2024-04-15 19:47:49 -0700

    Handle isFavorited prop

--- app/components/Pose.tsx
+++ app/components/Pose.tsx
@@ -1,17 +1,27 @@
 import { Link } from "@remix-run/react";
 import { PoseRecord } from "~/types";
 
-export default function Pose({ english_name, id, url_svg }: PoseRecord) {
+type PoseProps = {
+  isFavorited: boolean;
+} & PoseRecord;
+
+export default function Pose({
+  english_name,
+  id,
+  url_svg,
+  isFavorited,
+}: PoseProps) {
   return (
     <li>
-      <h3>{english_name}</h3>
+      <h3>
+        {isFavorited ? "❤️" : null} {english_name}
+      </h3>
       <img src={url_svg} alt={`Graphic of person doing ${english_name} pose`} />
       <div>
         <Link to={`/poses/${english_name}`}>Learn more</Link>
         <label>
           <button type="submit" name="pose_id" value={id}>
-            {" "}
-            Favorite pose
+            {isFavorited ? "Un-favorite pose" : "Favorite pose"}
           </button>
         </label>
       </div>
