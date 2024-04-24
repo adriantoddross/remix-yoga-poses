44b45b0ace279bbd15015a499f47a7f31dfe909a
Adrian Ross <adriantoddross@gmail.com>
2024-04-24 09:49:54 -0700

    Add variant to display link to signup button

--- app/components/Pose.tsx
+++ app/components/Pose.tsx
@@ -2,14 +2,36 @@ import { Link } from "@remix-run/react";
 import { PoseRecord } from "~/types";
 
 type PoseProps = {
-  isFavorited: boolean;
+  isFavorited?: boolean;
+  variant?: "favorite" | "signup";
 } & PoseRecord;
+
+function FavoritePoseButton({
+  isFavorited,
+  id,
+}: {
+  isFavorited: PoseProps["isFavorited"];
+  id: PoseProps["id"];
+}) {
+  return (
+    <label>
+      <button type="submit" name="pose_id" value={id}>
+        {isFavorited ? "Un-favorite pose" : "Favorite pose"}
+      </button>
+    </label>
+  );
+}
+
+function SignUpButton() {
+  return <Link to="/signup">Favorite Pose </Link>;
+}
 
 export default function Pose({
   english_name,
   id,
   url_svg,
   isFavorited,
+  variant = "favorite",
 }: PoseProps) {
   return (
     <li>
@@ -19,11 +41,12 @@ export default function Pose({
       <img src={url_svg} alt={`Graphic of person doing ${english_name} pose`} />
       <div>
         <Link to={`/poses/${english_name}`}>Learn more</Link>
-        <label>
-          <button type="submit" name="pose_id" value={id}>
-            {isFavorited ? "Un-favorite pose" : "Favorite pose"}
-          </button>
-        </label>
+
+        {variant === "favorite" ? (
+          <FavoritePoseButton isFavorited={isFavorited} id={id} />
+        ) : (
+          <SignUpButton />
+        )}
       </div>
     </li>
   );
