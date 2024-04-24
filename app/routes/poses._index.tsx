925bd5dded2c042c2b3c7ef6230fdcb8ccf8df0a
Adrian Ross <adriantoddross@gmail.com>
2024-04-24 09:50:19 -0700

    Code cleanup and refactor

--- app/routes/poses._index.tsx
+++ app/routes/poses._index.tsx
@@ -9,6 +9,9 @@ import { ChangeEvent, useCallback, useState } from "react";
 import Pose from "~/components/Pose";
 import { PoseCategory, PoseRecord } from "~/types";
 import { createSupabaseServerClient } from "../supabase/serverClient";
+import { getFavoritePoses } from "~/supabase/clientQueries";
+import { fetchCategories, fetchPoses } from "~/api/posesApi";
+import { useUser } from "~/root";
 
 export const meta: MetaFunction = () => {
   return [
@@ -44,6 +47,7 @@ export async function action({ request }: ActionFunctionArgs) {
   }
 
   return error;
+  // TODO: Handle error
 }
 
 export async function loader({ request }: LoaderFunctionArgs) {
@@ -52,26 +56,19 @@ export async function loader({ request }: LoaderFunctionArgs) {
     data: { session },
   } = await supabase.auth.getSession();
 
-  const poses = await fetch(
-    `${process.env.YOGA_API_BASE_URL}${process.env.YOGA_API_POSES}`
-  ).then((data) => data);
-
-  const categories = await fetch(
-    `${process.env.YOGA_API_BASE_URL}${process.env.YOGA_API_CATEGORIES}`
-  ).then((categoriesData) => categoriesData);
+  const [poses, categories, favoritePoses] = await Promise.all([
+    fetchPoses(),
+    fetchCategories(),
+    getFavoritePoses(supabase, session),
+  ]);
 
   const categoriesData: PoseCategory[] = await categories.json();
-
   const posesData: PoseRecord[] = await poses.json();
 
-  const { data, error } = await supabase
-    .from("favorite_poses")
-    .select()
-    .eq("user_id", session?.user.id);
-
-  if (error) {
+  if (favoritePoses?.error) {
     throw new Response(
-      error.message ?? "There was a problem with favoriting a pose.",
+      favoritePoses?.error.message ??
+        "There was a problem with favoriting a pose.",
       {
         status: 500,
       }
@@ -93,8 +90,7 @@ export async function loader({ request }: LoaderFunctionArgs) {
   return json({
     posesData,
     categoriesData,
-    favoritePoses: data?.map((pose) => pose.pose_id),
-    // favoritePosesError: error,
+    favoritePoses,
   });
 }
 
@@ -106,20 +102,14 @@ type PosesData = {
 export default function Category() {
   const { posesData, categoriesData, favoritePoses } =
     useLoaderData<typeof loader>();
+
+  const fetcher = useFetcher();
+  const { userIsLoggedIn } = useUser();
+
   const [poses, setPoses] = useState<PosesData>({
     poses: posesData,
     filters: new Set<string>(),
   });
-
-  const fetcher = useFetcher();
-  const isSubmitting = fetcher.state === "submitting";
-  const isFavoritePose = (poseId: number) => {
-    if (favoritePoses.length === 0) return false;
-
-    return fetcher.formData
-      ? fetcher.formData.get("pose_id") === poseId.toString()
-      : !!favoritePoses?.includes(poseId);
-  };
 
   const handleFilterChange = useCallback(
     (
@@ -190,20 +180,29 @@ export default function Category() {
         </fieldset>
       </div>
 
-      <fetcher.Form method="post">
+      {userIsLoggedIn ? (
+        <fetcher.Form method="post">
+          <ul>
+            {poses.poses.map((pose) => {
+              const { id } = pose;
+              return (
+                <Pose
+                  key={id}
+                  isFavorited={!!favoritePoses?.data?.includes(pose.id)}
+                  {...pose}
+                />
+              );
+            })}
+          </ul>
+        </fetcher.Form>
+      ) : (
         <ul>
           {poses.poses.map((pose) => {
             const { id } = pose;
-            return (
-              <Pose
-                key={id}
-                isFavorited={isSubmitting || isFavoritePose(pose.id)}
-                {...pose}
-              />
-            );
+            return <Pose key={id} variant="signup" {...pose} />;
           })}
         </ul>
-      </fetcher.Form>
+      )}
     </section>
   );
 }
